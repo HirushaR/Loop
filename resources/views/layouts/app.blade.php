355c5712fba2262fb492d0c718811884708fdbdd
HirushaR <hirusharandunu11@gmail.com>
2020-10-03 00:40:44 +0530

    index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,41 @@
     <link href="{{ asset('css/card.css') }}" rel="stylesheet">
 </head>
 <body>
+<nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0"
+     style="background-image: url(&quot;none&quot;);background-color: rgb(107,39,124);">
+    <div class="container-fluid d-flex flex-column p-0">
+        <!-- Start: Sidebar -->
+        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0"
+             style="background-color: rgb(107,39,124);background-image: url(&quot;none&quot;);margin-top: 50px;">
+            <div class="container-fluid d-flex flex-column p-0">
+                <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
+                   href="../home.php">
+
+                    <div class="sidebar-brand-text mx-3"><span><img src="../images/site/logohwite.png" alt=""></span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="nav navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="../home.php"><i
+                                class="fas fa-tachometer-alt"></i><span>&nbsp;Dashboard</span></a></li>
+                    <hr class="sidebar-divider">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="allCorses.php"><i  class="far fa-file-code"></i><span>&nbsp;All Courses</span></a>
+                        <a class="nav-link" href="../compiler/index.html"><i class="far fa-star-half"></i><span>&nbsp;Compilers&nbsp;</span></a>
+                        <a class="nav-link" href="../web_editor/index.html"><i class="far fa-star-half"></i><span>&nbsp;Live Editor&nbsp;</span></a>
+                        <a class="nav-link" href="followedcourse.php"><i class="far fa-star-half"></i><span>&nbsp;Followed Courses</span></a>
+                    </li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="completecourse.php"><i class="far fa-gem"></i><span>&nbsp;Complite Courses</span></a></li>
+                    <hr class="sidebar-divider">
+                </ul>
+                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0"
+                                                                    id="sidebarToggle" type="button"></button></div>
+            </div>
+        </nav>
+        <!-- End: Sidebar -->
+        <ul class="nav navbar-nav text-light" id="accordionSidebar"></ul>
+        <div class="text-center d-none d-md-inline"></div>
+    </div>
+</nav>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -81,4 +116,9 @@
         </main>
     </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+<script src="assets/js/script.min.js"></script>
 </html>
