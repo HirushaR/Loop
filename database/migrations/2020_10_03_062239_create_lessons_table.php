4bf1474e85b1776115bad8f5e100af66f7a05e3a
HirushaR <hirusharandunu11@gmail.com>
2020-10-03 11:57:27 +0530

    update lesson table

--- database/migrations/2020_10_03_062239_create_lessons_table.php
+++ database/migrations/2020_10_03_062239_create_lessons_table.php
@@ -15,6 +15,14 @@ class CreateLessonsTable extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
+            $table->integer('session_id');
+            $table->foreign('session_id')
+                ->references('id')
+                ->on('session')->onDelete('cascade');
+            $table->integer('lesson_number');
+            $table->string('topic');
+            $table->longText('body');
+            $table->longText('test_code');
             $table->timestamps();
         });
     }
