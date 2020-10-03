94a3dd58c742047541e9ebd99a9be81044b051f3
HirushaR <hirusharandunu11@gmail.com>
2020-10-03 12:12:53 +0530

    add user relationship with course

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    public function users()
+    {
+        //return $this->belongsToMany(RelatedModel, pivot_table_name, foreign_key_of_current_model_in_pivot_table, foreign_key_of_other_model_in_pivot_table);
+        return $this->belongsToMany(
+            User::class,
+            'user_course',
+            'course_id',
+            'user_id');
+    }
+
 }
