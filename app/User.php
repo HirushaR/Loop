9a8cf4d5f7d1e98de1cf284f627d3c28e2b5a22c
HirushaR <hirusharandunu11@gmail.com>
2020-10-03 12:14:53 +0530

    add Course relationship with users

--- app/User.php
+++ app/User.php
@@ -2,12 +2,22 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    public function course()
+    {
+        //return $this->belongsToMany(RelatedModel, pivot_table_name, foreign_key_of_current_model_in_pivot_table, foreign_key_of_other_model_in_pivot_table);
+        return $this->belongsToMany(
+            Course::class,
+            'user_course',
+            'user_id',
+            'course_id');
+    }
     use Notifiable;
 
     /**
